a0fd9d8df9ad3235bd895bb59a73dcb59773b19d
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-07-10 13:38:11 +0300

    add cardgame v2 files

---
+++ card-game-v3/src/main/java/sergei/cardgamev3/CardGameV3Application.java
@@ -0,0 +1,13 @@
+package sergei.cardgamev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGameV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardGameV3Application.class, args);
+    }
+
+}
---
+++ card-game-v3/src/test/java/sergei/cardgamev3/CardGameV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sergei.cardgamev3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGameV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
