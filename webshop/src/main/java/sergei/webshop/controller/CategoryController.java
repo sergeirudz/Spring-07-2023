0073c52e4474ec4fcb64baa8a9a7353d23b16261
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-07-18 13:42:09 +0300

    webshop add controllers

--- webshop/src/main/java/sergei/webshop/controller/CategoryController.java
+++ webshop/src/main/java/sergei/webshop/controller/CategoryController.java
@@ -1,9 +1,48 @@
 package sergei.webshop.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sergei.webshop.dto.CategoryDTO;
+import sergei.webshop.entity.Category;
+import sergei.webshop.service.CategoryService;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class CategoryController {
 
-    // KODUS: Kõikide võtmine, Lisamine, Kustutamine, Ühe võtmine, Muutmine
+    private final CategoryService categoryService;
+
+    @GetMapping("categories") // http://localhost:8080/categories
+    public ResponseEntity<List<Category>> getCategories() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping("categories")
+    public ResponseEntity<CategoryDTO> addCategory(
+            @RequestBody CategoryDTO categoryDTO) {
+        return categoryService.addOne(categoryDTO);
+    }
+
+    @DeleteMapping("categories")
+    public ResponseEntity<String> deleteCategory(
+            @RequestBody CategoryDTO category) {
+        return categoryService.deleteOne(category);
+    }
+
+    @GetMapping("categories/{id}")
+    public ResponseEntity<?> getCategory(
+            @PathVariable Long id) {
+        return categoryService.findOne(id);
+    }
+
+    @PatchMapping("categories/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(
+            @PathVariable Long id,
+            @RequestBody CategoryDTO categoryDTO) {
+        return categoryService.updateOne(id, categoryDTO);
+    }
 }
--- webshop/src/main/java/sergei/webshop/controller/PersonController.java
+++ webshop/src/main/java/sergei/webshop/controller/PersonController.java
@@ -1,7 +1,95 @@
 package sergei.webshop.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sergei.webshop.dto.PersonDTO;
+import sergei.webshop.entity.Person;
+import sergei.webshop.service.PersonService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class PersonController {
 
-    // KODUS: Kõikide võtmine, Lisamine???, Kustutamine, Ühe võtmine, Muutmine???
-    //                      korraga tuleb lisada ContactData ja siis Address
+    private final PersonService personService;
+
+    @GetMapping("persons") // http://localhost:8080/persons
+    public ResponseEntity<List<Person>> getAllPersons() {
+        return personService.findAll();
+    }
+
+    @PostMapping("persons")
+    public ResponseEntity<PersonDTO> addPerson(
+            @RequestBody PersonDTO personDTO) {
+        /*
+        {
+    "personalCode": "1",
+    "firstName": "John",
+    "lastName": "Doe",
+    "password": "password",
+    "contactData": {
+        "email": "john.doe1@example.com",
+        "phone": "+372555555",
+        "address": {
+            "country": "Estonia",
+            "county": "Harju",
+            "street": "123 Main Street",
+            "number": "12",
+            "postalIndex": "12345"
+        }
+    }
+}
+        * */
+        return personService.addOne(personDTO);
+    }
+
+    @DeleteMapping("person/{id}")
+    public ResponseEntity<PersonDTO> deletePerson(
+            @PathVariable Long id) {
+        return personService.deleteOne(id);
+    }
+
+    @GetMapping("/person")
+    public ResponseEntity<PersonDTO> getPerson(
+            @RequestParam(name = "id", required = false) Long id,
+            @RequestParam(name = "firstName", required = false) String firstName,
+            @RequestParam(name = "lastName", required = false) String lastName,
+            @RequestParam(name = "personalCode", required = false) String personalCode) {
+/*
+http://localhost:8080/person?firstName=John&lastName=Doe
+http://localhost:8080/person?personalCode=1
+http://localhost:8080/person?id=2
+* */
+
+        return personService.findOne(id, firstName, lastName, personalCode);
+    }
+
+    @PatchMapping("person/{id}")
+    public ResponseEntity<PersonDTO> updatePerson(
+            @RequestBody PersonDTO personDTO
+    ) {
+        /* Update all fields at once or only the ones that are provided
+        {
+          "id": 2,
+          "personalCode": "1234567890",
+          "firstName": "Updated First Name",
+          "lastName": "Updated Last Name",
+          "password": "updated_password",
+          "contactData": {
+            "email": "updated_email@example.com",
+            "phone": "9876543210",
+            "address": {
+              "country": "Updated Country",
+              "county": "Updated County",
+              "street": "Updated Street",
+              "number": "456",
+              "postalIndex": "67890"
+            }
+          }
+        }
+        * */
+        return personService.updateOne(personDTO);
+    }
 }
