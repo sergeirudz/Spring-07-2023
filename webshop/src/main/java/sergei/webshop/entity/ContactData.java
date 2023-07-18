8264998082fce3e2e3bfe1bd1dfdcba7bd98de60
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-07-18 06:37:33 +0300

    webshop add entities

---
+++ webshop/src/main/java/sergei/webshop/entity/ContactData.java
@@ -0,0 +1,24 @@
+package sergei.webshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ContactData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    private String phone;
+
+    @OneToOne
+    private Address address;
+}
