130ca4d00b7615fb6c16587b60ef599bbba72a39
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-07-25 07:11:29 +0300

    webshop add custom exceptions

--- webshop/src/main/java/sergei/webshop/exception/ControllerExceptionHandler.java
+++ webshop/src/main/java/sergei/webshop/exception/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package sergei.webshop.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.UnknownContentTypeException;
 
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -24,6 +26,31 @@ public class ControllerExceptionHandler {
         return getExceptionMessageResponseEntity(HttpStatus.BAD_REQUEST, message);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<ExceptionMessage> handleException(NoSuchElementException e) {
+        String message = e.getMessage();
+        return getExceptionMessageResponseEntity(HttpStatus.NOT_FOUND, message);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionMessage> handleException(NotEnoughInStockException e) {
+        String message = e.getMessage();
+        return getExceptionMessageResponseEntity(HttpStatus.BAD_REQUEST, message);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ExceptionMessage> handleException(DataIntegrityViolationException e) {
+        String message = e.getMessage();
+        return getExceptionMessageResponseEntity(HttpStatus.BAD_REQUEST, message);
+    }
+
+//     KÕIGE LÕPUS, KUI ON LIVE-i EELNE (kui on ülejäänud exceptionid kaetud)
+//    @ExceptionHandler
+//    public ResponseEntity<ExceptionMessage> handleException(Exception e) {
+//        String message = "General exception. Reason: Reason: " + e.getMessage();
+//        return getExceptionMessageResponseEntity(HttpStatus.BAD_REQUEST, message);
+//    }
+
     private static ResponseEntity<ExceptionMessage> getExceptionMessageResponseEntity(HttpStatus status, String exceptionMessage) {
         ExceptionMessage message = new ExceptionMessage();
         message.setDate(new Date());
@@ -31,14 +58,4 @@ public class ControllerExceptionHandler {
         message.setMessage(exceptionMessage);
         return new ResponseEntity<>(message, status);
     }
-
-    // KÕIGE LÕPUS, KUI ON LIVE-i EELNE (kui on ülejäänud exceptionid kaetud)
-//    @ExceptionHandler
-//    public ResponseEntity<ExceptionMessage> handleException(Exception e) {
-//        ExceptionMessage message = new ExceptionMessage();
-//        message.setDate(new Date());
-//        message.setHttpStatusCode(400);
-//        message.setMessage("General exception. Reason: " + e.getMessage());
-//        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-//    }
 }
---
+++ webshop/src/main/java/sergei/webshop/exception/NotEnoughInStockException.java
@@ -0,0 +1,7 @@
+package sergei.webshop.exception;
+
+public class NotEnoughInStockException extends Exception {
+    public NotEnoughInStockException(String message) {
+        super(message);
+    }
+}
