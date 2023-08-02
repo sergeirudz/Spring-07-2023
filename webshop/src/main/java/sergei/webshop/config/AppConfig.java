b17dc6b27f55df05f3fee4d95b3e181f9ff42127
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-08-02 14:23:04 +0300

    webshop add cors url

--- webshop/src/main/java/sergei/webshop/config/AppConfig.java
+++ webshop/src/main/java/sergei/webshop/config/AppConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -16,5 +18,21 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
+    }
+
+    //    @CrossOrigin(origins = "http://localhost:4200")
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(
+                    CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowedHeaders("*");
+//                        .allowCredentials(true);
+            }
+        };
     }
 }
