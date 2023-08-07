3529403f9fa4a9a76fa58356f7821c99bc94e9e6
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-08-07 19:57:13 +0300

    add login, signup & routes

--- webshop-frontend-angular2/src/app/app-routing.module.ts
+++ webshop-frontend-angular2/src/app/app-routing.module.ts
@@ -5,17 +5,24 @@ import { CartComponent } from './cart/cart.component';
 import { AddProductComponent } from './admin/add-product/add-product.component';
 import { EditProductComponent } from './admin/edit-product/edit-product.component';
 import { MaintainProductsComponent } from './admin/maintain-products/maintain-products.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const routes: Routes = [
+  { path: 'avaleht', redirectTo: '/', pathMatch: 'full' },
   { path: '', component: HomepageComponent },
   { path: 'ostukorv', component: CartComponent },
   { path: 'lisa-toode', component: AddProductComponent },
   { path: 'muuda-toode/:id', component: EditProductComponent },
   { path: 'halda-tooteid', component: MaintainProductsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
